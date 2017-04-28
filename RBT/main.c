434708d75094a22b8a09bd345bab334d1370d11f
Tabor Kvasnicka <kamakwazee@gmail.com>
2017-04-28 16:20:00 -0500

    Added RBT/main.c
    Signed-off-by: Tabor Kvasnicka <kamakwazee@gmail.com>

---
+++ RBT/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "rbt.h"
+
+
+int main(int argc, char** argv)
+{
+
+	if(argc < 2)
+	{
+		printf("Usage: %s [integers...]\n", argv[0]);
+		return 1;
+	}
+
+	RBT tree = NULL;
+
+	int nums[argc-1];
+	int n;
+	for(int i = 1; i < argc; i++)
+	{
+
+		n = atoi(argv[i]);
+		if(n == 0 && strcmp(argv[i], "0") != 0)
+		{
+			printf("Usage: %s [integers...]\n", argv[0]);
+			return 2;
+		}
+		nums[i-1] = n;
+
+	}
+
+	tree = insert(tree, nums[0]);
+	for(int i = 1; i < argc-1; i++)
+	{
+		insert(tree, nums[i]);
+	}
+	//tree = removeNode(tree, nums[2]);
+	Traverse(tree);
+	dispose(tree);
+	return 0;
+
+}
